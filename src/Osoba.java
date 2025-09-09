6a8691610a208f858cec27c0b5e322ce6104aa94
f4Mythical <grynka.adam@gmail.com>
2025-09-09 08:29:06 +0200

    osoba klasa

---
+++ src/Osoba.java
@@ -0,0 +1,36 @@
+public abstract class Osoba {
+    private String imie;
+    private String nazwisko;
+
+    public Osoba(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko= nazwisko;
+    }
+    public Osoba(){
+    this.imie="jan";
+    this.nazwisko="kowalski";
+    }
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    @Override
+    public String toString() {
+        return "Osoba{" +
+                "imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'' +
+                '}';
+    }
+}
