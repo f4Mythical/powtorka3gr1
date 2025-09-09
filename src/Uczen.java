11dda8159a4ddb60904b3236c0bb06bb5b01af51
f4Mythical <grynka.adam@gmail.com>
2025-09-09 08:57:24 +0200

    uczen klasa

--- src/Uczen.java
+++ src/Uczen.java
@@ -1,1 +1,34 @@
+public class Uczen extends Osoba {
+    private static int liczbaUczniow = 0;
+    private int numerUcznis = 0;
 
+    public Uczen(String imie, String nazwisko) {
+        super(imie, nazwisko);
+        liczbaUczniow++;
+        numerUcznis=liczbaUczniow;
+
+    }
+
+    public static int getLiczbaUczniow() {
+        return liczbaUczniow;
+    }
+
+    public static void setLiczbaUczniow(int liczbaUczniow) {
+        Uczen.liczbaUczniow = liczbaUczniow;
+    }
+
+    public int getNumerUcznis() {
+        return numerUcznis;
+    }
+
+    public void setNumerUcznis(int numerUcznis) {
+        this.numerUcznis = numerUcznis;
+    }
+
+    @Override
+    public String toString() {
+        return "Uczen{ imie" +getImie()+" nazwisko " + getNazwisko() +
+                "numerUcznis=" + numerUcznis +
+                '}';
+    }
+}
