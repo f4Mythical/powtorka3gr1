dfadf0c0ecc992e243766e1d3e00c01231890307
f4Mythical <grynka.adam@gmail.com>
2025-09-09 09:07:06 +0200

    testowanie klasy uczen

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    Uczen uczen = new Uczen("Okay","Fredek");
+        System.out.println("liczba uczniow" + Uczen.getLiczbaUczniow());
+    Uczen uczen2 = new Uczen("po","supert");
+        System.out.println("liczba uczniow" + Uczen.getLiczbaUczniow());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Uczen uczen3 = new Uczen("Ok","meta");
+        System.out.println("liczba uczniow" + Uczen.getLiczbaUczniow());
+        System.out.println(uczen3);
     }
+
 }
